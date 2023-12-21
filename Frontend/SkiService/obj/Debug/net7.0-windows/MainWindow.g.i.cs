dac00ed9fe1cc1009c494bfb4eab6e748f9035e8
Davud-Ponjevic <davud.ponjevic@outlook.com>
2023-12-21 13:46:02 +0100

    Backend von 295 eingefügt

--- Frontend/SkiService/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ Frontend/SkiService/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -100,7 +100,7 @@ namespace SkiService {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/SkiService;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/SkiService;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
